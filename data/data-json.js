2a1e925ceaa6b3c6db8ded44396de7611b72152b
Melanie Caballero <mel_caballero@icloud.com>
2025-02-23 00:25:02 +0100

    Revert "Dev"

---
+++ data/data-json.js
@@ -0,0 +1,254 @@
+const temas =  {
+  tema : [
+    {
+      'tituloTema' : 'Introducción',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Introducción',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'introduccion',
+        },
+        {
+          'tituloSubtema' : 'Destinatarios',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'destinatarios',
+        },
+        {
+          'tituloSubtema' : 'Consideraciones',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'consideraciones',
+        },
+        {
+          'tituloSubtema' : 'Contactos',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'contactos',
+        },
+        {
+          'tituloSubtema' : 'Biblioteca',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'biblioteca',
+        },
+      ],
+    },
+    {
+      'tituloTema' : 'Accesibilidad Web',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'DTD',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'dtd',
+        },
+        {
+          'tituloSubtema' : 'Idioma',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'idioma',
+        },
+        {
+          'tituloSubtema' : 'Título',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'titulo',
+        },
+        {
+          'tituloSubtema' : 'Viewport',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'viewport',
+        },
+        {
+          'tituloSubtema' : 'Semántica',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'semantica',
+        },
+        {
+          'tituloSubtema' : 'Imágenes',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'imagenes',
+        },
+        {
+          'tituloSubtema' : 'Características sensoriales',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'caracteristicasSensoriales',
+        },
+        {
+          'tituloSubtema' : 'Uso del color',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'usoColor',
+        },
+        {
+          'tituloSubtema' : 'Contraste de color',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'contrasteColor',
+        },
+        {
+          'tituloSubtema' : 'Configuración personalizada',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'configuracionPersonalizada',
+        },
+        {
+          'tituloSubtema' : 'Orientación',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'orientacion',
+        },
+        {
+          'tituloSubtema' : 'Navegación por teclado',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'navegacionTeclado',
+        },
+        {
+          'tituloSubtema' : 'Gestos',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'gestos',
+        },
+        {
+          'tituloSubtema' : 'Hover y Focus',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'hoverFocus',
+        },
+        {
+          'tituloSubtema' : 'Mensajes de estado',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'mensajesEstado',
+        },
+        {
+          'tituloSubtema' : 'Formularios',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'formularios',
+        },
+        {
+          'tituloSubtema' : 'Errores',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'errores',
+        },
+        {
+          'tituloSubtema' : 'Subtítulos',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'subtitulos',
+        },
+        {
+          'tituloSubtema' : 'Enlaces de interés',
+          'ruta' : 'assets/principiosA11Y/',
+          'nombre' : 'enlacesA11y',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'WCAG',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'WCAG Introducción',
+          'ruta' : 'assets/wcag/',
+          'nombre' : 'wcag-introduccion',
+        },
+        {
+          'tituloSubtema' : 'WCAG Principios',
+          'ruta' : 'assets/wcag/',
+          'nombre' : 'wcag-principios',
+        },
+        {
+          'tituloSubtema' : 'WCAG 2.2.',
+          'ruta' : 'assets/wcag/',
+          'nombre' : 'wcag22',
+        },
+        {
+          'tituloSubtema' : 'WCAG 3.0. Draft',
+          'ruta' : 'assets/wcag/',
+          'nombre' : 'wcag30draft',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'Techniques',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Wcag Quickref',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'success_criteria',
+        },
+        {
+          'tituloSubtema' : 'Errores comunes',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'commonErrors',
+        },
+        {
+          'tituloSubtema' : 'PDF Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'pdf-techniques',
+        },
+        {
+          'tituloSubtema' : 'ARIA Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'wcag_aria_techniques',
+        },
+        {
+          'tituloSubtema' : 'Client Side Script Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'wcag_client-side-script_techniques',
+        },
+        {
+          'tituloSubtema' : 'CSS Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'wcag_css_techniques',
+        },
+        {
+          'tituloSubtema' : 'Failures Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'wcag_failures_techniques',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'UNE Web',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Árbol de decisiones',
+          'ruta' : 'assets/une/',
+          'nombre' : 'arboldecisiones',
+        },
+        {
+          'tituloSubtema' : 'Anexo B',
+          'ruta' : 'assets/une/',
+          'nombre' : 'anexob',
+        },
+        {
+          'tituloSubtema' : 'Metodología informe',
+          'ruta' : 'assets/une/',
+          'nombre' : 'metodologia',
+        },
+        {
+          'tituloSubtema' : 'Muestra',
+          'ruta' : 'assets/une/',
+          'nombre' : 'muestra',
+        },
+        {
+          'tituloSubtema' : 'Tabla detallada',
+          'ruta' : 'assets/une/',
+          'nombre' : 'tabladetallada',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'UNE Móvil',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Árbol de decisiones',
+          'ruta' : 'assets/mobile/',
+          'nombre' : 'arboldecisiones',
+        },
+        {
+          'tituloSubtema' : 'Herramientas',
+          'ruta' : 'assets/mobile/',
+          'nombre' : 'tools-mobile',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'Talleres',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Creando Equipos Inclusivos',
+          'ruta' : 'assets/talleres/',
+          'nombre' : 'creando-equipos-inclusivos',
+        }
+      ]
+    }
+  ]
+}
