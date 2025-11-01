8d60c11e8f723e81032bcdb46b9b55cf3508136e
Melanie Caballero <melanie.caballero.pastor@gmail.com>
2025-11-01 04:26:53 +0100

    Subida

---
+++ data/data-json.js
@@ -0,0 +1,229 @@
+const temas =  {
+  tema : [
+    {
+      'tituloTema' : 'Introducción',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Introducción',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'introduccion',
+        },
+        {
+          'tituloSubtema' : 'Destinatarios',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'destinatarios',
+        },
+        {
+          'tituloSubtema' : 'Consideraciones',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'consideraciones',
+        },
+        {
+          'tituloSubtema' : 'Derechos Humanos',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'derechos-humanos',
+        },
+        {
+          'tituloSubtema' : 'Derechos de Personas con Discapacidad',
+          'ruta' : 'assets/intro/',
+          'nombre' : 'derechos-personas-discapacidad',
+        },
+      ],
+    },
+    {
+      'tituloTema' : 'Accesibilidad Web',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'DTD',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'dtd',
+        },
+        {
+          'tituloSubtema' : 'Idioma',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'idioma',
+        },
+        {
+          'tituloSubtema' : 'Título',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'titulo',
+        },
+        {
+          'tituloSubtema' : 'Viewport',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'viewport',
+        },
+        {
+          'tituloSubtema' : 'Semántica',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'semantica',
+        },
+        {
+          'tituloSubtema' : 'Imágenes',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'imagenes',
+        },
+        {
+          'tituloSubtema' : 'Características sensoriales',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'caracteristicas-sensoriales',
+        },
+        {
+          'tituloSubtema' : 'Uso del color',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'uso-color',
+        },
+        {
+          'tituloSubtema' : 'Contraste de color',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'contraste-color',
+        },
+        {
+          'tituloSubtema' : 'Configuración personalizada',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'configuracion-personalizada',
+        },
+        {
+          'tituloSubtema' : 'Orientación',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'orientacion',
+        },
+        {
+          'tituloSubtema' : 'Navegación por teclado',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'navegacion-teclado',
+        },
+        {
+          'tituloSubtema' : 'Gestos',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'gestos',
+        },
+        {
+          'tituloSubtema' : 'Hover y Focus',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'hover-focus',
+        },
+        {
+          'tituloSubtema' : 'Mensajes de estado',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'mensajes-estado',
+        },
+        {
+          'tituloSubtema' : 'Formularios',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'formularios',
+        },
+        {
+          'tituloSubtema' : 'Errores',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'errores',
+        },
+        {
+          'tituloSubtema' : 'Subtítulos',
+          'ruta' : 'assets/principios-a11y/',
+          'nombre' : 'subtitulos',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'WCAG',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'WCAG Introducción',
+          'ruta' : 'assets/wcag/',
+          'nombre' : 'wcag-introduccion',
+        },
+        {
+          'tituloSubtema' : 'WCAG Principios',
+          'ruta' : 'assets/wcag/',
+          'nombre' : 'wcag-principios',
+        },
+        {
+          'tituloSubtema' : 'WCAG 2.2.',
+          'ruta' : 'assets/wcag/',
+          'nombre' : 'wcag-22',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'Techniques',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Succes Criteria',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'success-criteria',
+        },
+        {
+          'tituloSubtema' : 'Common Errors',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'common-errors',
+        },
+        {
+          'tituloSubtema' : 'PDF Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'pdf-techniques',
+        },
+        {
+          'tituloSubtema' : 'ARIA Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'aria-techniques',
+        },
+        {
+          'tituloSubtema' : 'Client Side Script Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'client-side-script-techniques',
+        },
+        {
+          'tituloSubtema' : 'CSS Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'css-techniques',
+        },
+        {
+          'tituloSubtema' : 'Failures Techniques',
+          'ruta' : 'assets/techniques/',
+          'nombre' : 'failures-techniques',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'UNE Web',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Anexo B',
+          'ruta' : 'assets/une-web/',
+          'nombre' : 'anexo-b',
+        },
+        {
+          'tituloSubtema' : 'Metodología informe',
+          'ruta' : 'assets/une-web/',
+          'nombre' : 'metodologia',
+        },
+        {
+          'tituloSubtema' : 'Muestra',
+          'ruta' : 'assets/une-web/',
+          'nombre' : 'muestra',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'UNE Móvil',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Herramientas',
+          'ruta' : 'assets/une-mobile/',
+          'nombre' : 'tools-mobile',
+        }
+      ]
+    },
+    {
+      'tituloTema' : 'Talleres',
+      'subtema' : [
+        {
+          'tituloSubtema' : 'Creando Equipos Inclusivos',
+          'ruta' : 'assets/talleres/',
+          'nombre' : 'creando-equipos-inclusivos',
+        }
+      ]
+    }
+  ]
+}
